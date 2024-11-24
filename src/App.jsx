6e010aba43332af128203ad1f1330f47ecd485bc
jeeva <jeelion22@gmail.com>
2024-11-24 19:07:15 +0530

    remove unsued packages and its imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Dashboard from "./Dashboard";
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import Chatbot from "./Chatbot";
+
 import "./App.css";
 
 const App = () => {
