f979ff490d1caf816a5c0e6d3cb9112d016c3fdc
jeeva <jeelion22@gmail.com>
2024-12-03 14:04:12 +0530

    update with components for each btns

--- src/features/dashboard/DefineProduct.jsx
+++ src/features/dashboard/DefineProduct.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import "./DefineProduct.css";
+import EditComponents from "../../EditComponents";
+import RemoveComponents from "../../RemoveComponents";
+import RemoveAllComponents from "../../RemoveAllComponents";
+import CreateProductDefinition from "../../CreateProductDefinition";
 
-const DefineProduct = () => {
-  return <div>DefineProduct</div>;
+const componentMapping = {
+  CreateProductDefinition,
+  EditComponents,
+  RemoveComponents,
+  RemoveAllComponents,
+};
+
+const DefineProduct = ({ productID }) => {
+  const [activeComponent, setActiveComponet] = useState(null);
+
+  const handleEdit = (componentName) => {
+    setActiveComponet(componentName);
+  };
+
+  const ActiveComponent = componentMapping[activeComponent];
+
+  return (
+    <div className="product-definition-container">
+      <h5>IoT Device</h5>
+      <div className="device-control-btns edit-tools">
+        <i
+          className={`bi bi-plus-square ${
+            activeComponent === "CreateProductDefintion" ? "active" : ""
+          }`}
+          title="create product definition"
+          onClick={() => handleEdit("CreateProductDefinition")}
+        ></i>
+
+        <i
+          className={`bi bi-dash-square ${
+            activeComponent === "RemoveComponents" ? "active" : ""
+          }`}
+          title="remove components"
+          onClick={() => handleEdit("RemoveComponents")}
+        ></i>
+
+        <i
+          className={`bi bi-pencil-square ${
+            activeComponent === "EditComponents" ? "active" : ""
+          }`}
+          title="edit components"
+          onClick={() => handleEdit("EditComponents")}
+        ></i>
+        <i
+          className={`bi bi-trash3 ${
+            activeComponent === "RemoveAllComponents" ? "active" : ""
+          }`}
+          title="remove all device components"
+          onClick={() => handleEdit("RemoveAllComponents")}
+        ></i>
+      </div>
+
+      {ActiveComponent && <ActiveComponent productID={productID} />}
+    </div>
+  );
 };
 
 export default DefineProduct;
